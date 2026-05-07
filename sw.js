14b97aca147557fd719bc0ac3b7654c60184a68d
gtexido <gtexido@gmail.com>
2026-05-07 20:42:59 -0300

    Limpieza visual premium v2.2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mi-rutina-v21-premium-squad';
+const CACHE = 'mi-rutina-v22-premium-squad';
 const APP_SHELL = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', event => {
