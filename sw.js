a258d104231cfb74f292c693a06440cf739a3629
gtexido <gtexido@gmail.com>
2026-05-06 23:49:17 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mi-rutina-v10';
+const CACHE = 'mi-rutina-v11';
 const APP_SHELL = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', event => {
