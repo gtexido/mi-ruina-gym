ef054a67fa0bb7ea7366873e8c1e29c7208d8a96
gtexido <gtexido@gmail.com>
2026-05-06 21:55:45 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'rutina-v8-json';
+const CACHE = 'rutina-v20260506-vertical-sheets';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
